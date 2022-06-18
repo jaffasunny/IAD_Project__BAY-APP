28be9472a221c86838d27794c38d0a473ec2fe01
Syed Jaffer Sunny <52747254+jaffasunny@users.noreply.github.com>
2022-06-18 06:00:25 -0700

    Upload report image functionality

--- src/pages/ReportPage/ReportPage.jsx
+++ src/pages/ReportPage/ReportPage.jsx
@@ -9,6 +9,7 @@ import {
   TextareaAutosize,
   Typography,
 } from "@mui/material";
+
 import "./ReportPage.css";
 
 import { ArrowBackIos } from "@mui/icons-material";
@@ -16,7 +17,15 @@ import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 
 const ReportPage = () => {
-  const [files, setFiles] = useState([]);
+  const [selectedFile, setSelectedFile] = useState();
+  const [formData, setFormData] = useState({
+    reportText: "",
+    reportImage: "",
+    accept: false,
+  });
+
+  const [reportText, setReportText] = useState("");
+  const [accept, setAccept] = useState(false);
 
   const navigate = useNavigate();
 
@@ -31,6 +40,34 @@ const ReportPage = () => {
       },
     },
   });
+
+  const handleReportUploadImage = (e) => {
+    let reader = new FileReader();
+
+    // Server Side
+    if (e.target.files[0]) {
+      reader.readAsDataURL(e.target.files[0]);
+
+      reader.onload = (e) => {
+        setSelectedFile({ file: e.target.result });
+      };
+    }
+  };
+
+  const onFileUpload = (e) => {
+    e.preventDefault();
+
+    // now send this image data on server
+    const formData = new FormData();
+
+    formData.append("reportFile", selectedFile, selectedFile.name);
+
+    // Request made to the backend api
+    // Send formData object
+    // axios.post("api/uploadfile", formData);
+
+    // Server Side end
+  };
 
   return (
     <form className='reportPage'>
@@ -55,7 +92,7 @@ const ReportPage = () => {
             aria-label='maximum height'
             placeholder='Type here'
             style={{
-              width: "100%",
+              width: "90%",
               height: 150,
               borderRadius: "26px",
               border: "1px solid #9D9D9D",
@@ -64,6 +101,7 @@ const ReportPage = () => {
               overflow: "scroll",
             }}
             className='reportPage__textArea'
+            onChange={(e) => setReportText(e.target.value)}
           />
 
           <label htmlFor='contained-button-file'>
@@ -73,6 +111,8 @@ const ReportPage = () => {
               multiple
               type='file'
               sx={{ display: "none" }}
+              onChange={handleReportUploadImage}
+              value=''
             />
             <Button
               variant='contained'
@@ -82,20 +122,40 @@ const ReportPage = () => {
                 background: "linear-gradient(180deg, #FBAC12 0%, #D29111 100%)",
                 borderRadius: "27px",
                 textTransform: "capitalize",
-              }}
-              onChange={(e) => {
-                let file = e.target.files;
-                console.log(file);
               }}>
               Upload Image
             </Button>
           </label>
+
+          {selectedFile && (
+            <div className='uploadImage'>
+              <img
+                style={{
+                  width: "50%",
+                  boxShadow:
+                    "0px 159.534px 355.398px rgba(0, 0, 0, 0.04), 0px 66.6495px 148.477px rgba(0, 0, 0, 0.0287542), 0px 35.634px 79.3827px rgba(0, 0, 0, 0.0238443), 0px 19.9761px 44.5013px rgba(0, 0, 0, 0.02), 0px 10.6092px 23.6343px rgba(0, 0, 0, 0.0161557), 0px 4.41472px 9.83477px rgba(0, 0, 0, 0.0112458)",
+                  borderRadius: "23.6932px",
+                }}
+                src={selectedFile.file}
+                alt=''
+              />
+
+              <Button
+                className='uploadImage__deletebtn'
+                onClick={() => {
+                  setSelectedFile();
+                }}>
+                Delete
+              </Button>
+            </div>
+          )}
 
           <FormControlLabel
             sx={{ mb: 5 }}
             value='false'
             control={<Radio />}
             label='Agree to Terms and Conditions'
+            onChange={() => setAccept(true)}
           />
 
           <div className='button__group flex__between'>
@@ -116,10 +176,8 @@ const ReportPage = () => {
                 px: 5,
                 py: 1,
               }}
-              onClick={() => {
-                navigate("/login");
-              }}>
-              Login
+              onSubmit={onFileUpload}>
+              Send
             </Button>
           </div>
         </ThemeProvider>
