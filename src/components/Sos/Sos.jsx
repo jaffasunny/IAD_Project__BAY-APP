9025b9a2a432d3433dd2282f48c8f02f7d34278d
Syed Jaffer Sunny <52747254+jaffasunny@users.noreply.github.com>
2022-06-19 12:27:22 -0700

    SOS component

--- src/components/Sos/Sos.jsx
+++ src/components/Sos/Sos.jsx
@@ -1,20 +1,56 @@
 import React from "react";
-import swal from "sweetalert";
 import { useLongPress, LongPressDetectEvents } from "use-long-press";
 
 import "./Sos.css";
 
+import withReactContent from "sweetalert2-react-content";
+import Swal from "sweetalert2";
+
 const Sos = () => {
-  const callback = React.useCallback(() => {
-    swal("SOS Call Successful", "Please stay at your location", "success", {
-      button: true,
+  const callback = React.useCallback((e) => {
+    // e.preventDefault();
+
+    const MySwal = withReactContent(Swal);
+
+    MySwal.fire({
+      showClass: {
+        popup: "animate__animated animate__fadeInDown",
+      },
+      hideClass: {
+        popup: "animate__animated animate__fadeOutUp",
+      },
+      title: <strong>Finding Help</strong>,
+      showDenyButton: true,
+      confirmButtonText: "911",
+      denyButtonText: "Emergency Contacts",
+      imageUrl:
+        "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80",
+    }).then((result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        window.location = "tel:911";
+      } else if (result.isDenied) {
+        // var contactsManager = navigator.contacts;
+        // const supported =
+        //   "contacts" in navigator && "ContactsManager" in window;
+        window.location = "tel:";
+      }
     });
   }, []);
 
   const bind = useLongPress(true ? callback : null, {
-    onStart: () => console.log("Press started"),
-    onFinish: () => console.log("Long press finished"),
-    onCancel: () => console.log("Press cancelled"),
+    // onStart: (e) => {
+    //   e.preventDefault();
+    //   // console.log("Press started");
+    // },
+    onCancel: (e) => {
+      e.preventDefault();
+      // console.log("Press cancelled");
+    },
+    onFinish: (e) => {
+      e.preventDefault();
+      // console.log("Long press finished");
+    },
     //onMove: () => console.log("Detected mouse or touch movement"),
     threshold: 3000,
     captureEvent: true,
